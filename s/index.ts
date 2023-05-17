41bff7721bbb0b2d5b2344ce44290958eccf2ab1
Chase Moskal <chasemoskal@gmail.com>
2023-05-17 14:03:06 -0700

    fix: missing index re-exports

--- s/index.ts
+++ s/index.ts
@@ -1,5 +1,6 @@
 
 export * from "./build/website.js"
+export * from "./build/webpage.js"
 export * from "./build/types/template_basics.js"
 export * from "./build/types/output_logger.js"
 
@@ -16,6 +17,7 @@ export * from "./html/webpage.js"
 export * from "./utils/escape_html.js"
 export * from "./utils/escape_regex.js"
 export * from "./utils/find_files.js"
-export * from "./utils/write_file.js"
 export * from "./utils/hash_versioner.js"
+export * from "./utils/path.js"
+export * from "./utils/write_file.js"
 
