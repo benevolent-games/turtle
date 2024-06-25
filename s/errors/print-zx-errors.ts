aa92c0ed933ef674bf839c6b74ca6c060092f11c
Chase Moskal <chasemoskal@gmail.com>
2024-06-25 01:02:50 -0700

    fix: zx error printing

--- s/errors/print-zx-errors.ts
+++ s/errors/print-zx-errors.ts
@@ -1,18 +1,16 @@
 
 import {ProcessOutput} from "zx"
-import {color} from "@benev/argv"
+import {ExecutionError} from "@benev/argv"
 
 export async function printZxErrors(fn: () => Promise<void>) {
 	try {
 		await fn()
 	}
 	catch (error) {
-		if (error instanceof ProcessOutput) {
-			console.error(
-				color.red(error.text())
-			)
-		}
-		else console.error("error:", error)
+		if (error instanceof ProcessOutput)
+			throw new ExecutionError(error.text())
+		else
+			throw new ExecutionError(`error: ${JSON.stringify(error)}`)
 	}
 }
 
